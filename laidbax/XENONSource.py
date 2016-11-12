83efb199b38d44f5f0499ffae743d94cc0a68f4d
Jelle Aalbers <jelleaalbers@gmail.com>
2016-11-12 15:51:14 +0100

    Option for simplified ER/NR band model

--- laidbax/XENONSource.py
+++ laidbax/XENONSource.py
@@ -4,10 +4,12 @@ from blueice.source import MonteCarloSource
 from blueice.utils import InterpolateAndExtrapolate1D
 from multihist import Hist1d
 
+from .signals import simulate_signals
+
 
 class XENONSource(MonteCarloSource):
     """A Source in a XENON-style experiment"""
-    energy_distribution = None      # Histdd of rate /kg /keV /day.
+    energy_distribution = None  # Histdd of rate /kg /keV /day.
 
     def __init__(self, config, *args, **kwargs):
         # Defaults for config settings
@@ -27,6 +29,99 @@ class XENONSource(MonteCarloSource):
         self.events_per_day = h.histogram.sum() * self.config['fiducial_mass'] * (h.bin_edges[1] - h.bin_edges[0])
 
         super().compute_pdf()
+
+    def simulate(self, n_events):
+        """Simulate n_events from this source."""
+        c = self.config
+
+        energies = self.energy_distribution.get_random(n_events)
+
+        # Get the mean number of "base quanta" produced
+        n_quanta = c['base_quanta_yield'] * energies
+        n_quanta = np.random.normal(n_quanta,
+                                    np.sqrt(c['base_quanta_fano_factor'] * n_quanta),
+                                    size=n_events)
+
+        # 0 or negative numbers of quanta give trouble with the later formulas.
+        # Store which events are bad, set them to 1 quanta for now, then zero these events later.
+        bad_events = n_quanta < 1
+        n_quanta = np.clip(n_quanta, 1, float('inf'))
+
+        n_quanta = np.round(n_quanta).astype(np.int32)
+        photons_produced, electrons_produced = self.quanta_to_photons_electrons(energies, n_quanta)
+
+        # "Remove" bad events (see above); actual removal happens at the very end of the function
+        photons_produced[bad_events] *= 0
+        electrons_produced[bad_events] *= 0
+
+        d = simulate_signals(c, photons_produced, electrons_produced, energies)
+
+        return d
+
+    def quanta_to_photons_electrons(self, energies, n_quanta):
+        raise NotImplementedError
+
+
+class SimplifiedXENONSource(XENONSource):
+
+    def quanta_to_photons_electrons(self, energies, n_quanta):
+        c = self.config
+        if c['recoil_type'] == 'nr':
+            # Account for quanta getting lost as heat
+            p_detectable = c['nr_p_detectable_a'] * np.log10(energies) + c['nr_p_detectable_b']
+            n_quanta = np.random.binomial(n_quanta, p_detectable)
+
+        # Simple lin-log model of probability of becoming an electron
+        p_becomes_electron = c[c['recoil_type'] + '_p_electron_a'] * np.log10(energies) + c[c['recoil_type'] + '_p_electron_b']
+
+        # Extra fluctuation (according to LUX due to fluctuation in recombination probability)
+        fluctuation = c['p_%s_electron_fluctuation' % c['recoil_type']]
+        if fluctuation != 0:
+            p_becomes_electron = np.random.normal(p_becomes_electron, fluctuation)
+        p_becomes_electron = np.clip(p_becomes_electron, 0, 1)
+
+        # Sample the actual numbers binomially
+        electrons_produced = np.random.binomial(n_quanta, p=p_becomes_electron)
+        return n_quanta - electrons_produced, electrons_produced
+
+
+
+class RegularXENONSource(XENONSource):
+    """Old-style MC which constructs photon and electron yields from LEff, QY, etc"""
+
+    def quanta_to_photons_electrons(self, energies, n_quanta):
+        c = self.config
+        n_events = len(energies)
+
+        p_becomes_photon = energies * self.yield_at(energies, c['recoil_type'], 'photon') / n_quanta
+        p_becomes_electron = energies * self.yield_at(energies, c['recoil_type'], 'electron') / n_quanta
+
+        if c['recoil_type'] == 'er':
+            # Apply extra recombination fluctuation (NEST tritium paper / Atilla Dobii's thesis)
+            p_becomes_electron = np.random.normal(p_becomes_electron,
+                                                  p_becomes_electron * c['recombination_fluctuation'],
+                                                  size=n_events)
+            p_becomes_electron = np.clip(p_becomes_electron, 0, 1)
+            p_becomes_photon = 1 - p_becomes_electron
+            n_quanta = np.round(n_quanta).astype(np.int)
+
+        elif c['recoil_type'] == 'nr':
+            # For NR some quanta get lost in heat.
+            # Remove them and rescale the p's so we can use the same code as for ERs after this.
+            p_becomes_detectable = p_becomes_photon + p_becomes_electron
+            if p_becomes_detectable.max() > 1:
+                raise ValueError("p_detected max is %s??!" % p_becomes_detectable.max())
+            p_becomes_photon /= p_becomes_detectable
+            n_quanta = np.round(n_quanta).astype(np.int)
+            n_quanta = np.random.binomial(n_quanta, p_becomes_detectable)
+
+        else:
+            raise ValueError('Bad recoil type %s' % c['recoil_type'])
+
+        photons_produced = np.random.binomial(n_quanta, p_becomes_photon)
+        electrons_produced = n_quanta - photons_produced
+
+        return photons_produced, electrons_produced
 
     def yield_at(self, energies, recoil_type, quantum_type):
         """Return the yield in quanta/kev for the given energies (numpy array, in keV),
@@ -79,134 +174,3 @@ class XENONSource(MonteCarloSource):
 
         else:
             raise RuntimeError("invalid recoil type %s" % recoil_type)
-
-    def simulate(self, n_events):
-        """Simulate n_events from this source."""
-        c = self.config
-
-        # Store everything in a structured array:
-        d = np.zeros(n_events, dtype=[('energy', np.float),
-                                      ('r2', np.float),
-                                      ('theta', np.float),
-                                      ('z', np.float),
-                                      ('p_photon_detected', np.float),
-                                      ('p_electron_detected', np.float),
-                                      ('electrons_produced', np.int),
-                                      ('photons_produced', np.int),
-                                      ('electrons_detected', np.int),
-                                      ('s1_photons_detected', np.int),
-                                      ('s2_photons_detected', np.int),
-                                      ('s1_photoelectrons_produced', np.int),
-                                      ('s2_photoelectrons_produced', np.int),
-                                      ('s1', np.float),
-                                      ('s2', np.float),
-                                      ('cs1', np.float),
-                                      ('cs2', np.float),
-                                      ('source', np.int),  # Not set in this function
-                                      ('magic_cs1', np.float)])
-
-        # If we get asked to simulate 0 events, return the empty array immediately
-        if not len(d):
-            return d
-
-        d['energy'] = self.energy_distribution.get_random(n_events)
-
-        # Sample the positions and relative light yields
-        if c['spatial_distribution'] == 'uniform':
-            d['r2'] = np.random.uniform(0, c['fiducial_volume_radius']**2, n_events)
-            d['theta'] = np.random.uniform(0, 2*np.pi, n_events)
-            d['z'] = np.random.uniform(c['ficudial_volume_zmin'], c['ficudial_volume_zmax'], size=n_events)
-            rel_lys = c['s1_relative_ly_map'].lookup(d['r2'], d['z'])
-        else:
-            raise NotImplementedError("Only uniform sources supported for now...")
-
-        # Get the light & charge collection efficiency
-        d['p_photon_detected'] = c['ph_detection_efficiency'] * rel_lys
-        d['p_electron_detected'] = np.exp(d['z'] / c['v_drift']/ c['e_lifetime'])   # No minus: z is negative
-
-        # Get the mean number of "base quanta" produced
-        n_quanta = c['base_quanta_yield'] * d['energy']
-        n_quanta = np.random.normal(n_quanta,
-                                    np.sqrt(c['base_quanta_fano_factor'] * n_quanta),
-                                    size=n_events)
-
-        # 0 or negative numbers of quanta give trouble with the later formulas.
-        # Store which events are bad, set them to 1 quanta for now, then zero them later.
-        bad_events = n_quanta < 1
-        n_quanta = np.clip(n_quanta, 1, float('inf'))
-
-        p_becomes_photon = d['energy'] * self.yield_at(d['energy'], c['recoil_type'], 'photon') / n_quanta
-        p_becomes_electron = d['energy'] * self.yield_at(d['energy'], c['recoil_type'], 'electron') / n_quanta
-
-        if c['recoil_type'] == 'er':
-            # Apply extra recombination fluctuation (NEST tritium paper / Atilla Dobii's thesis)
-            p_becomes_electron = np.random.normal(p_becomes_electron,
-                                                  p_becomes_electron * c['recombination_fluctuation'],
-                                                  size=n_events)
-            p_becomes_electron = np.clip(p_becomes_electron, 0, 1)
-            p_becomes_photon = 1 - p_becomes_electron
-            n_quanta = np.round(n_quanta).astype(np.int)
-
-        elif c['recoil_type'] == 'nr':
-            # For NR some quanta get lost in heat.
-            # Remove them and rescale the p's so we can use the same code as for ERs after this.
-            p_becomes_detectable = p_becomes_photon + p_becomes_electron
-            if p_becomes_detectable.max() > 1:
-                raise ValueError("p_detected max is %s??!" % p_becomes_detectable.max())
-            p_becomes_photon /= p_becomes_detectable
-            n_quanta = np.round(n_quanta).astype(np.int)
-            n_quanta = np.random.binomial(n_quanta, p_becomes_detectable)
-
-        else:
-            raise ValueError('Bad recoil type %s' % c['recoil_type'])
-
-        d['photons_produced'] = np.random.binomial(n_quanta, p_becomes_photon)
-        d['electrons_produced'] = n_quanta - d['photons_produced']
-
-        # "Remove" bad events (see above); actual removal happens at the very end of the function
-        d['photons_produced'][bad_events] = 0
-        d['electrons_produced'][bad_events] = 0
-
-        # Detection efficiency
-        d['s1_photons_detected'] = np.random.binomial(d['photons_produced'], d['p_photon_detected'])
-        d['electrons_detected'] = np.random.binomial(d['electrons_produced'], d['p_electron_detected'])
-
-        # S2 amplification
-        d['s2_photons_detected'] = np.random.poisson(d['electrons_detected'] * c['s2_gain'])
-
-        # PMT response
-        for si in ('s1', 's2'):
-            # Convert photons to photoelectrons, taking double photoelectron emission into account
-            d[si + '_photoelectrons_produced'] = d[si + '_photons_detected'] + \
-                                                   np.random.binomial(d[si + '_photons_detected'],
-                                                                      c['double_pe_emission_probability'])
-
-            # Convert photoelectrons to measured pe
-            d[si] = np.random.normal(d[si + '_photoelectrons_produced'],
-                                     np.clip(c['pmt_gain_width'] * np.sqrt(d[si + '_photoelectrons_produced']),
-                                             1e-9,   # Normal freaks out if sigma is 0...
-                                             float('inf')))
-
-        # Get the corrected S1 and S2, assuming our posrec + correction map is perfect
-        # Note this does NOT assume the analyst knows the absolute photon detection efficiency:
-        # photon detection efficiency / p_photon_detected is just the relative light yield at the position.
-        # p_electron_detected is known exactly (since it only depends on the electron lifetime)
-        s1_correction = c['ph_detection_efficiency'] / d['p_photon_detected']
-        d['cs1'] = d['s1'] * s1_correction
-        d['cs2'] = d['s2'] / d['p_electron_detected']
-
-        # Assuming we know the total number of photons detected (perfect hit counting),
-        # give the ML estimate of the number of photons produced.
-        d['magic_cs1'] = d['s1_photons_detected'] * s1_correction
-
-        # Remove events without an S1 or S1
-        if c['require_s1']:
-            # One photons detected doesn't count as an S1 (since it isn't distinguishable from a dark count)
-            d = d[d['s1_photons_detected'] >= 2]
-            d = d[d['s1'] > c['s1_area_threshold']]
-
-        if c['require_s2']:
-            d = d[d['electrons_detected'] >= 1]
-            d = d[d['s2'] > c['s2_area_threshold']]
-
-        return d
---
+++ laidbax/signals.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+
+def simulate_signals(config, n_photons, n_electrons, energies=None):
+    """Simulate results due to n_photons and n_electrons produced. Returns numpy structured array.
+    energies is not used, but will be included in the results for your convenience if you pass it.
+
+    config must be a config dictionary containing at least:
+     - s1_relative_ly_map
+     - ph_detection_efficiency
+     - double_pe_emission_probability
+     - e_lifetime
+     - v_drift
+     - s2_gain
+     - fiducial_volume_radius, fiducial_volume_zmin, fiducial_volume_zmax
+    and optionally
+     - require_s1, require_s2 (booleans, default true)
+     - s1_area_threshold, s2_area_threshold (minimum areas in pe for S1 and S2)
+     - spatial_distribution (currently only 'uniform' supported)
+    """
+    c = config
+    n_events = len(n_photons)
+
+    # Store everything in a structured array:
+    d = np.zeros(n_events, dtype=[
+        ('source', np.int),
+        # Set here:
+        ('energy', np.float),
+        ('r2', np.float),
+        ('theta', np.float),
+        ('z', np.float),
+        ('p_photon_detected', np.float),
+        ('p_electron_detected', np.float),
+        ('electrons_produced', np.int),
+        ('photons_produced', np.int),
+        ('electrons_detected', np.int),
+        ('s1_photons_detected', np.int),
+        ('s1_photoelectrons_produced', np.int),
+        ('s1', np.float),
+        ('s2', np.float),
+        ('cs1', np.float),
+        ('cs2', np.float),
+    ])
+
+    if energies is not None:
+        d['energy'] = energies
+
+    # If we get asked to simulate 0 events, return the empty array immediately
+    if not len(d):
+        return d
+
+    # Fill in the inputs from the higher-level code
+    d['photons_produced'] = n_photons
+    d['electrons_produced'] = n_electrons
+
+    # Sample the positions and relative light yields
+    if c.get('spatial_distribution', 'uniform') == 'uniform':
+        d['r2'] = np.random.uniform(0, c['fiducial_volume_radius'] ** 2, n_events)
+        d['theta'] = np.random.uniform(0, 2 * np.pi, n_events)
+        d['z'] = np.random.uniform(c['ficudial_volume_zmin'], c['ficudial_volume_zmax'], size=n_events)
+        rel_lys = c['s1_relative_ly_map'].lookup(d['r2'], d['z'])
+    else:
+        raise NotImplementedError("Only uniform sources supported for now...")
+
+    # Get the light & charge collection efficiency
+    d['p_photon_detected'] = c['ph_detection_efficiency'] * rel_lys
+    d['p_electron_detected'] = c.get('electron_extraction_efficiency', 1) * \
+                               np.exp(d['z'] / c['v_drift'] / c['e_lifetime'])  # No minus: z is negative
+
+    # Detection efficiency
+    d['s1_photons_detected'] = np.random.binomial(d['photons_produced'], d['p_photon_detected'])
+    d['electrons_detected'] = np.random.binomial(d['electrons_produced'], d['p_electron_detected'])
+
+    # S2 amplification
+    d['s2'] = np.random.poisson(d['electrons_detected'] * c['s2_gain'])
+
+    # S1 response
+    d['s1_photoelectrons_produced'] = d['s1_photons_detected'] + np.random.binomial(d['s1_photons_detected'],
+                                                                                    c['double_pe_emission_probability'])
+    d['s1'] = np.random.normal(d['s1_photoelectrons_produced'],
+                               np.clip(c['pmt_gain_width'] * np.sqrt(d['s1_photoelectrons_produced']),
+                                       1e-9,   # Normal freaks out if sigma is 0...
+                                       float('inf')))
+
+    # Get the corrected S1 and S2, assuming our posrec + correction map is perfect
+    # Note this does NOT assume the analyst knows the absolute photon detection efficiency:
+    # photon detection efficiency / p_photon_detected is just the relative light yield at the position.
+    # p_electron_detected is known exactly (since it only depends on the electron lifetime)
+    s1_correction = c['ph_detection_efficiency'] / d['p_photon_detected']
+    d['cs1'] = d['s1'] * s1_correction
+    d['cs2'] = d['s2'] / d['p_electron_detected']
+
+    # Remove events without an S1 or S1
+    if c.get('require_s1', True):
+        # One photons detected doesn't count as an S1 (since it isn't distinguishable from a dark count)
+        d = d[d['s1_photons_detected'] >= 2]
+        d = d[d['s1'] > c.get('s1_area_threshold', 0)]
+
+    if c.get('require_s2', True):
+        d = d[d['electrons_detected'] >= 1]
+        d = d[d['s2'] > c.get('s2_area_threshold', 0)]
+
+    return d
