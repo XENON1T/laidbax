c6c57a2700513504b6ebf5a539176354384753f0
Jelle Aalbers <jelleaalbers@gmail.com>
2017-03-22 16:26:50 +0100

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 requirements = open('requirements.txt').read().splitlines()
 
 setup(name='laidbax',
-      version='0.2.0',
+      version='0.3.0',
       description='Statistical XENON1T Analysis for the lazy analyst.',
       long_description=readme + '\n\n' + history,
       author='Jelle Aalbers',
