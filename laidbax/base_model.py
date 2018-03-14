4d80de9a1f281dd0926d34f02aa77f5c569e81fd
Jelle Aalbers <jelleaalbers@gmail.com>
2018-03-14 11:17:31 +0100

    Add tutorial, cleanup

--- laidbax/base_model.py
+++ laidbax/base_model.py
@@ -112,7 +112,7 @@ config = dict(
     s2_area_threshold=150,
 
     # Coincidence requirement on S1
-    s1_min_photons_detected = 3,
+    s1_min_photons_detected=3,
 
     # Bias (primarily due to self-trigger)
     s1_bias='x1t_s1_bias_Feb26.csv',
